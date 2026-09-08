9a3eff45675792c7965ff9a815355be73632d7b4
indresh kumawat <120265646+Indreshkumawat@users.noreply.github.com>
2026-09-09 02:02:03 +0530

    Time: 0 ms (100%), Space: 42 MB (81.92%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,36 @@
+class Solution {
+    // int solve(int m,int n,int i,int j){
+    //      if(i == m -1 && j == n-1){
+    //         return 1;
+    //     }
+    //      if(i > m-1 || j > n -1){
+    //         return 0;
+    //     }
+       
+    //     return solve(m,n,i+1,j) + solve(m,n,i,j+1);
+    // }
+    public int uniquePaths(int m, int n) {
+        //return solve(m,n,0,0);
+
+        int dp[][] = new int[m][n];
+
+        dp[0][0] = 1;
+
+        for(int i = 1;i<m;i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1;i<n;i++){
+            dp[0][i] = 1;
+        }
+
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+
+        return dp[m-1][n-1];
+
+
+    }
+}
