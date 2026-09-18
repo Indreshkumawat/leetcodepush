31e05031871e9b93c020fbc606555c2eb80dc8ab
indresh kumawat <120265646+Indreshkumawat@users.noreply.github.com>
2026-09-18 22:57:58 +0530

    Time: 20 ms (20.1%), Space: 70.3 MB (8.91%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,27 +1,39 @@
 class Solution {
-    int solve(int n,int screen,int clipboard){
-        if(screen == n){
+    int dp[][];
+
+    int solve(int n, int screen, int clipboard) {
+        if (screen == n) {
             return 0;
         }
-        if(screen > n){
+        if (screen > n) {
             return 1000000;
+        }
+
+        if (dp[screen][clipboard] != -1) {
+            return dp[screen][clipboard];
         }
         int copy = 1000000;
         int paste = 1000000;
-        if(screen != clipboard){
-            copy = 1 + solve(n,screen,screen);
+        if (screen != clipboard) {
+            copy = 1 + solve(n, screen, screen);
         }
-        if(clipboard > 0){
-            paste = 1 + solve(n,screen+clipboard,clipboard);
+        if (clipboard > 0) {
+            paste = 1 + solve(n, screen + clipboard, clipboard);
         }
 
-        return Math.min(copy,paste);
-        
+        return dp[screen][clipboard] = Math.min(copy, paste);
+
     }
+
     public int minSteps(int n) {
-        if(n==1){
+        if (n == 1) {
             return 0;
         }
-        return solve(n,1,0);
+        dp = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(n, 1, 0);
     }
 }
