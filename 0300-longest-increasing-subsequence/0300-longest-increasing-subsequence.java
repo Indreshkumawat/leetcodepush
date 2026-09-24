6e77e6edf865e40edbfa335df9c4c54133278cf8
indresh kumawat <120265646+Indreshkumawat@users.noreply.github.com>
2026-09-25 00:42:52 +0530

    Time: 45 ms (43.21%), Space: 45.6 MB (98.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,46 @@
 class Solution {
-    int dp[][];
-    int solve(int[] nums,int index,int preIndex){
-        if(index < 0){
-            return 0;
-        }
-        if(dp[index][preIndex] != -1){
-            return dp[index][preIndex];
-        }
-        int take = 0;
-        int notTake = 0;
-        if(preIndex == nums.length || nums[index] < nums[preIndex]){
-            take = 1 + solve(nums,index - 1,index);
-        }
-        notTake = solve(nums,index-1,preIndex);
-        return dp[index][preIndex] = Math.max(take,notTake);
-    }
+    // int dp[][];
+    // int solve(int[] nums,int index,int preIndex){
+    //     if(index < 0){
+    //         return 0;
+    //     }
+    //     if(dp[index][preIndex] != -1){
+    //         return dp[index][preIndex];
+    //     }
+    //     int take = 0;
+    //     int notTake = 0;
+    //     if(preIndex == nums.length || nums[index] < nums[preIndex]){
+    //         take = 1 + solve(nums,index - 1,index);
+    //     }
+    //     notTake = solve(nums,index-1,preIndex);
+    //     return dp[index][preIndex] = Math.max(take,notTake);
+    // }
     public int lengthOfLIS(int[] nums) {
-        int size = nums.length;
-        dp = new int[size][size+1];
-        for(int i = 0;i<size;i++){
-            Arrays.fill(dp[i],-1);
+        // int size = nums.length;
+        // dp = new int[size][size+1];
+        // for(int i = 0;i<size;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+
+        // return solve(nums,size-1,size);
+
+        int dp[] = new int[nums.length];
+
+        Arrays.fill(dp,1);
+        int maxi = 1;
+        for(int i = 0;i<nums.length;i++){
+            int j = 0;
+            while(j<i){
+                if(nums[i]>nums[j]){
+                    dp[i] = Math.max(dp[i],dp[j] + 1);
+                    maxi = Math.max(dp[i],maxi);
+                }
+                j++;
+            }
         }
 
-        return solve(nums,size-1,size);
+        
+
+        return maxi;
     }
 }
